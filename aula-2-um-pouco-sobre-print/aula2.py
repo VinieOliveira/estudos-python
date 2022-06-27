922f1cc2eb619d464a14eb8b94efaf142a725665
VinieOliveira <vinicius_oliveiragons@hotmail.com>
2022-06-27 17:36:49 -0300

    resolvendo erros de pasta duplicada

--- aula-2-um-pouco-sobre-print/aula2.py
+++
@@ -1,4 +0,0 @@
-print('vinicius', 123) #separar os argumentos por virgula por padrão já vai com espaço.
-print('Vinicius', 'Oliveira', sep='-', end=' ')
-print('Vinicius', 'e', 'a vida', sep='-', end='')
-Print ('não usar letra maiúscula na função, python diferencia maiúscula de minuscula')
