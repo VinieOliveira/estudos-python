1a180eba1fe3bd0d1c4b60913843748d4577be0a
VinieOliveira <vinicius_oliveiragons@hotmail.com>
2022-06-27 17:22:05 -0300

    renomeando pasta 2 que estava errada

---
+++ aula2-um-pouco-sobre-print/aula2.py
@@ -0,0 +1,4 @@
+print('vinicius', 123) #separar os argumentos por virgula por padrão já vai com espaço.
+print('Vinicius', 'Oliveira', sep='-', end=' ')
+print('Vinicius', 'e', 'a vida', sep='-', end='')
+Print ('não usar letra maiúscula na função, python diferencia maiúscula de minuscula')
