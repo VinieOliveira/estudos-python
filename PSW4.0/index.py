9898a5ae6bcbf88ec740608270988f696934dc6c
VinieOliveira <vinicius_oliveiragons@hotmail.com>
2022-07-04 21:32:15 -0300

    estudo aquecimento

--- PSW4.0/index.py
+++ PSW4.0/index.py
@@ -1,11 +1,14 @@
-nota1 = input("Digite sua primeira nota: ")
+nota1 = input("Digite sua primeira nota: ") #aqui também poderia colocar o input dentro do int para converter
 nota2 = input("Digite sua segunda nota: ")
 print("Vamos ver sua média...")
-media = (int(nota1) + int(nota2)) /2
+media = (float(nota1) + float(nota2)) /2
 
-if media > 8:
+if media >= 8:
     print ("Parabéns, você passou! Sua média é {}!".format(media))
+elif media >= 6:
+    print(f"recuperação, sua média é {media}")
 else:
     print ("Parabéns, você não passou! HAHA, sua média é {}".format(media))
 
     #A AULA PAROU EM 31M
+    
---
+++ PSW4.0/teste.py
@@ -0,0 +1,7 @@
+n = 0
+
+while n < 11:
+    resultado = n * 5
+    print(f'{n} x 5 = {resultado}')  
+    n = n+1
+#parei em 1h4m
